ed84abe6d6217bccf6205d26c66414147e0a170d
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2020-03-10 14:52:09 -0300

    Update gitignore

--- setup.py
+++ setup.py
@@ -2,28 +2,34 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(
-  name = 'TwitterSDK',         # How you named your package folder (MyLib)
-  packages = ['TwitterSDK'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'SDK to help Twitter API consume',   # Give a short description about your library
-  author = 'Jessé Souza',                   # Type in your name
-  author_email = 'jesse.neto@ccc.ufcg.edu.br',      # Type in your E-Mail
-  url = 'https://github.com/user/jessescn',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jessescn/TwitterSDK/archive/0.1.tar.gz',    # I explain this later on
-  keywords = ['Twitter', 'API','Python', 'SDK'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'requests'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+DESCRIPTION = 'A Twitter SDK to help API consume.'
+URL = 'https://github.com/user/jessescn'
+DOWNLOAD_URL = 'https://github.com/jessescn/TwitterSDK/archive/0.1.tar.gz'
+
+VERSION = '0.1'
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-  ]
+]
+
+setup(
+  name='TwitterSDK',   
+  packages=['TwitterSDK'],
+  version=VERSION,
+  license='MIT',
+  description=DESCRIPTION,
+  author='Jessé Souza',
+  author_email='jesse.neto@ccc.ufcg.edu.br',
+  url=URL,
+  download_url=DOWNLOAD_URL,
+  keywords=['Twitter', 'API','Python', 'SDK'],
+  install_requires=['requests'],
+  classifiers=CLASSIFIERS
 )
