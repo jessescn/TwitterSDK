126515015ac75dd0c7f62446bcd9b07ed100105f
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2020-03-10 14:25:59 -0300

    Added files to publish package in PyPi

---
+++ TwitterSDK/__init__.py
@@ -0,0 +1,2 @@
+from TwitterSDK.twitter import Twitter
+from TwitterSDK.auth import OAuth2Handler
---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from distutils.core import setup
+setup(
+  name = 'TwitterSDK',         # How you named your package folder (MyLib)
+  packages = ['TwitterSDK'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'SDK to help Twitter API consume',   # Give a short description about your library
+  author = 'Jessé Souza',                   # Type in your name
+  author_email = 'jesse.neto@ccc.ufcg.edu.br',      # Type in your E-Mail
+  url = 'https://github.com/user/jessescn',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['Twitter', 'API','Python', 'SDK'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'requests'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
